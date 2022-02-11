3b189140aedfca40085dfd01047b9fc583cdc0d3
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2022-02-11 12:17:40 -0300

    Construindo Meu Portifolio
    Ainda em desenvolvimento !
    Faltando construir um rodapé
    Pagina Sobre .
    E colocar umas animações e interações

---
+++ index.js
@@ -0,0 +1,10 @@
+const arrayBreno = [1,2,4,3,6,5,0,9,8,7,]
+const arraySamara = [12,13,14,15]
+const arrayJuncao = arrayBreno.concat(arraySamara)
+
+
+
+
+for (i = 0; i < arrayBreno.length; i ++ ){
+    console.log(arrayBreno.sort()[i])
+}
